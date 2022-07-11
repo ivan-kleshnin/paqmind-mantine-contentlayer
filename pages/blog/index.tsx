16ea74942c3d7c2922c8c15f3226e9b412b3bf65
Ivan Kleshnin <ivan@paqmind.com>
2022-07-11 11:08:30 +0200

    Fix Blog and Testimonials pages

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,10 @@
-import {Group, Stack, Title} from "@mantine/core"
+import {Box, Container, Title} from "@mantine/core"
 import {Page, Post, allPages, allPosts} from "contentlayer/generated"
 import {ParsedUrlQuery} from "querystring"
 import {GetStaticProps} from "next"
 import Head from "next/head"
 import {useMDXComponent} from "next-contentlayer/hooks"
-import {HorizontalCard, Typography} from "components"
+import {Group, HorizontalCard, Stack, Typography} from "components"
 import * as U from "lib/utils"
 
 // BlogPage
@@ -20,28 +20,34 @@ export default function BlogPage({blog, posts}: BlogPageProps) : JSX.Element {
     <Head>
       <title>{blog.title}</title>
     </Head>
-    <main>
-      <Typography>
-        <h1>{blog.title}</h1>
-        <MDXContent components={{Group}}/>
-      </Typography>
+    <Box sx={{backgroundColor: "#eee"}} pt="2rem" pb="2.5rem">
+      <Container size={BlogPage.layoutSize}>
+        <main>
+          <Title>{blog.title}</Title>
+          <Typography>
+            <MDXContent components={{Group}}/>
+          </Typography>
 
-      <Title order={2} mt={32} mb={24}>All Posts</Title>
-      <Stack spacing={24}>
-        {posts.map((post, i) => (
-          <HorizontalCard
-            key={i}
-            title={post.title}
-            intro={post.intro.html}
-            postedAt={post.createdAt}
-            tags={post.tags}
-            url={post.url}
-          />
-        ))}
-      </Stack>
-    </main>
+          <Title order={2} mt="2rem" mb=".5rem">All Posts</Title>
+          <Stack spacing="1rem">
+            {posts.map((post, i) =>
+              <HorizontalCard
+                key={i}
+                title={post.title}
+                intro={post.intro.html}
+                postedAt={post.createdAt}
+                tags={post.tags}
+                url={post.url}
+              />
+            )}
+          </Stack>
+        </main>
+      </Container>
+    </Box>
   </>
 }
+
+BlogPage.layoutSize = "md" as const
 
 // SSR /////////////////////////////////////////////////////////////////////////////////////////////
 type Payload = {
--- pages/testimonials.tsx
+++ pages/testimonials.tsx
@@ -1,10 +1,14 @@
+import {Box, Container, Title} from "@mantine/core"
 import {Page, Testimonial, allPages, allTestimonials} from "contentlayer/generated"
 import {ParsedUrlQuery} from "querystring"
 import {GetStaticProps} from "next"
 import Head from "next/head"
 import {useMDXComponent} from "next-contentlayer/hooks"
-import {Button} from "components"
+import {CommentCard, Group, Stack, Typography} from "components"
+import BlogPage from "pages/blog"
+import * as U from "lib/utils"
 
+// TestimonialsPage
 type TestimonialsPageProps = Payload
 
 export default function TestimonialsPage({page, testimonials}: TestimonialsPageProps) : JSX.Element {
@@ -14,40 +18,32 @@ export default function TestimonialsPage({page, testimonials}: TestimonialsPageP
     <Head>
       <title>{page.title}</title>
     </Head>
-    <main>
-      <h1>{page.title}</h1>
+    <Box sx={{backgroundColor: "#eee"}} pt="2rem" pb="2.5rem">
+      <Container size={BlogPage.layoutSize}>
+        <main>
+          <Title>{page.title}</Title>
+          <Typography>
+            <MDXContent components={{Group}}/>
+          </Typography>
 
-      <div>
-        <MDXContent components={{Button}}/>
-      </div>
-
-      <h2>Testimonials</h2>
-      {testimonials.map((testimonial, i) => (
-        <TestimonialCard key={i} testimonial={testimonial}/>
-      ))}
-    </main>
+          <Title order={2} mt="2rem" mb=".5rem">Testimonials</Title>
+          <Stack spacing="1rem">
+            {testimonials.map((testimonial, i) => (
+              <CommentCard
+                key={i}
+                postedAt="today"
+                body={testimonial.body.html}
+                author={{name: "JohnDoe", image: "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80"}}
+              />
+            ))}
+          </Stack>
+        </main>
+      </Container>
+    </Box>
   </>
 }
 
-// TestimonialCard
-type TestimonialCardProps = {
-  testimonial: Testimonial
-}
-
-function TestimonialCard({testimonial} : TestimonialCardProps) : JSX.Element {
-  return <>
-    <div>
-      <div>
-        <time dateTime={testimonial.createdAt}>
-          {new Date(testimonial.createdAt).toLocaleDateString()}
-        </time>
-      </div>
-      <div>
-        <div dangerouslySetInnerHTML={{__html: testimonial.body.html}}/>
-      </div>
-    </div>
-  </>
-}
+TestimonialsPage.layoutSize = "md" as const
 
 // SSR /////////////////////////////////////////////////////////////////////////////////////////////
 type Payload = {
@@ -58,14 +54,14 @@ type Payload = {
 type Params = ParsedUrlQuery
 
 export const getStaticProps: GetStaticProps<Payload, Params> = async () => {
-  const url = "/"
+  const url = "/testimonials"
   const page = allPages.find(p => p.url == url)
 
   if (!page) {
     throw new Error("...")
   }
 
-  const testimonials = [...allTestimonials]
+  const testimonials = [...allTestimonials].sort(U.byCreatedAtDesc)
 
   return {
     props: {
