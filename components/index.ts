3aad98fba569bba691d731b98a4fb49b734d33d5
Ivan Kleshnin <ivan@paqmind.com>
2022-07-11 10:51:59 +0200

    Export fixed Group and Stack
    Should accept rem-based spacing!

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,33 @@
+import * as React from "react"
+
 export * from "./Button"
 export * from "./Carousel"
 export * from "./CommentCard"
 export * from "./HorizontalCard"
 export * from "./Link"
 export * from "./Typography"
+
+import * as M from "@mantine/core"
+
+// Group (override)
+export type GroupProps = Omit<M.GroupProps, "spacing"> & {
+  spacing?: M.MantineNumberSize | string
+}
+
+export const Group = M.Group as React.ForwardRefExoticComponent<GroupProps & React.RefAttributes<HTMLDivElement>>
+
+// Stack (override)
+export type StackProps = Omit<M.StackProps, "spacing"> & {
+  spacing?: M.MantineNumberSize | string
+}
+
+export const Stack = M.Stack as React.ForwardRefExoticComponent<StackProps & React.RefAttributes<HTMLDivElement>>
+
+// Grid (override)
+export type GridProps = Omit<M.GridProps, "gutter"> & {
+  gutter?: M.MantineNumberSize | string
+}
+
+export const Grid = M.Grid as M.ForwardRefWithStaticComponents<GridProps, {
+  Col: typeof M.Col
+}>
